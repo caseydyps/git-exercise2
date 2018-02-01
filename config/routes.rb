9b5316ad7004d521bd81162ceed7434aef0b032f
caseydyps <caseydyps@gmail.com>
2018-02-01 23:02:16 +0800

    update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-<<<<<<< HEAD
   root "user#index"
-=======
-  root "users#index"
->>>>>>> e6e061ff514747b390ffe6c8c726eb8ff83d7e77
+
 end
